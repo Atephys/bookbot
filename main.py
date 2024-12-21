619cbc52f66cc1c36719946ac0357ae97fef5e30
Atephys <73765578+Atephys@users.noreply.github.com>
2024-12-21 04:53:15 +0100

    better way to read frankenstein

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def main():
-    with open("books/frankenstein.txt") as f:
-        frankenstein = f.read()
-        print(frankenstein)
+    path_to_book = "books/frankenstein.txt"
+    book_text = get_book_text(path_to_book)
+    print(book_text)
+
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
+        return f.read()
 
 main()
