84311229ffb36e0b6f0297de6c35343eae7b65e3
Atephys <73765578+Atephys@users.noreply.github.com>
2024-12-21 04:58:24 +0100

    get word count

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
     path_to_book = "books/frankenstein.txt"
     book_text = get_book_text(path_to_book)
-    print(book_text)
+    word_count = get_word_count(book_text)
+    print(word_count)
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
         return f.read()
+
+def get_word_count(text):
+    word_list = text.split()
+    return len(word_list)
 
 main()
